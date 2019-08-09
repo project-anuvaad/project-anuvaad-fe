e970c3e0dfddcc0f340962d704c0e3b6f38e47f5
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-08-09 12:18:31 +0530

    basepath added

--- src/configs/apigw.js
+++ src/configs/apigw.js
@@ -3,13 +3,13 @@ const configs = {
     AUTH_ENDPOINT: 'oauth2/authorize',
     LOGOUT_ENDPOINT: 'logout',
     POST_LOGOUT_URL:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : ''),
-    RETURN_TO:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
+    RETURN_TO:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/dev/callback',
     RESPONSE_TYPE: 'response_type=token&scope=view',
     //Test
     // CLIENT_ID: 'client_id=6d810055-185e-4799-baba-c78693da6134',
     //Prod
     CLIENT_ID: 'client_id=a5c43a83-e94c-4810-8fbf-543af7452a6e',
-    REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
+    REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/dev/callback',
     
 };
 

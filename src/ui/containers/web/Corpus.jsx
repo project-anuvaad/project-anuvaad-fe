1fe95aa910131fae11290b3e0c9b94fb205ccae8
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-08-12 15:57:33 +0530

    accuracy changed

--- src/ui/containers/web/Corpus.jsx
+++ src/ui/containers/web/Corpus.jsx
@@ -306,10 +306,10 @@ class Corpus extends React.Component {
 
                     <TableCell align="left">
                         {row.alignment_accuracy === 'GAPFILLER\n' || row.alignment_accuracy === 'GALECHURCH\n' ?
-                            <span variant="fab" style={{ width: '35px', height: '35px', borderRadius: '50%', display: 'inline-block', backgroundColor: 'red' }} />
+                            'Less than 30%'
                             :
-                            (row.alignment_accuracy === 'BLEU\n' ? <span style={{ width: '35px', height: '35px', borderRadius: '50%', display: 'inline-block', backgroundColor: 'yellow' }}>    </span>
-                                : null)}
+                            (row.alignment_accuracy === 'BLEU\n' ? '30% to 50%'
+                                : (row.alignment_accuracy && row.alignment_accuracy.indexOf('-') > 0 ? Math.round(100*(50+row.alignment_accuracy.split('\n')[0].split('-')[1]*100/2))/ 100 + '%': ''))}
                     </TableCell>
 
                     <TableCell>
